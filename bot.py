ae85a3b145c88c75d9eaf334bc9a995da1cb3260
DmitrySezionov <65273381+DmitrySezionov@users.noreply.github.com>
2020-06-02 17:59:14 +0300

    Renew

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ TOKEN = '1297554794:AAHuCSd-vol-lOPPOcBqhjdGihWgIW719sU'
 bot = telebot.TeleBot(TOKEN)
 
 keyboard_start = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard_start.row('Add new task', 'My Tasks', 'Settings')
+keyboard_start.row('My Tasks', 'Add new task', 'Settings')
 
 keyboard_add = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard_add.row('Task Today', 'All Tasks')
+keyboard_add.row('Task Today', 'Task for 3 days' 'All Tasks')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -16,9 +16,9 @@ def start_message(message):
 
 
 
-
+#MY TASK
 def correct_text1(sentence):
-    is_correct = re.match(r'^[My Task]+$', sentence)
+    is_correct = re.match(r'^[My Tasks]+$', sentence)
     return bool(is_correct)
 
 def preprocess_text1(sentence):
@@ -28,7 +28,7 @@ def preprocess_text1(sentence):
 
 
 
-
+#ADD NEW TASK
 def correct_text2(sentence):
     is_correct = re.match(r'^[Add new task]+$', sentence)
     return bool(is_correct)
@@ -42,15 +42,23 @@ def preprocess_text2(sentence):
 
 
 @bot.message_handler(content_types=['text'])
-def add_text(message):
+def mytask_text(message):
     cid = message.chat.id
 
     if correct_text1(preprocess_text1(message.text)):
-        bot.send_message(message.chat.id, 'Your tasks')
-    elif correct_text2(preprocess_text2(message.text)):
-        bot.send_message(message.chat.id, 'Put your task name', reply_markup=keyboard_add)
+        bot.send_message(message.chat.id, '''Your task today:
+        \nIf you want to view all task, choose from menu.''', reply_markup=keyboard_add)
     else:
-        bot.send_message(message.chat.id, 'NONONON')
+        bot.send_message(message.chat.id, 'I don`t know that')
+
+    # if correct_text1(preprocess_text1(message.text)):
+    #     bot.send_message(message.chat.id, 'Your tasks', reply_markup=keyboard_add)
+    # elif correct_text2(preprocess_text2(message.text)):
+    #     bot.send_message(message.chat.id, 'Put your task name')
+    # else:
+    #     bot.send_message(message.chat.id, 'NONONON')
+
+
 
 if __name__ == '__main__':
     bot.polling()
