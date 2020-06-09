3743fc8222b918d411753521fd8393afb8f50d00
DmitrySezionov <65273381+DmitrySezionov@users.noreply.github.com>
2020-06-09 17:16:28 +0300

    Add config file

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import re
 import telebot
+import config
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -8,6 +10,9 @@ bot = telebot.TeleBot(TOKEN)
 def start_message(message):
     bot.send_message(message.chat.id, 'Hi, i`m Planer Bot. Use me, to create notifications')
 
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id, 'So, i didn`t know')
 
 if __name__ == '__main__':
     bot.polling()
