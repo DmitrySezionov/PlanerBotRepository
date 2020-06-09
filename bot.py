ab40be517f2a17e764424b419a690c610cc84587
DmitrySezionov <65273381+DmitrySezionov@users.noreply.github.com>
2020-06-09 17:50:07 +0300

    Add next steps

--- bot.py
+++ bot.py
@@ -16,9 +16,27 @@ class User:
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, '''Привет, я твой карманный помощник! Я много чего умею, поверь!
-    \nДля того, чтобы начать со мной работать - <b>введи своё имя</b>, чтобы я зарегистрировал тебя!
-    \nЕсли хочешь узнать, как я работаю, то загляни в команду /help.''', parse_mode='html')
+    msg = bot.send_message(message.chat.id, '''Привет, я твой карманный помощник! Я много чего умею, поверь!
+    \nДля того, чтобы начать со мной работать - <b>введи своё имя</b>, чтобы я зарегистрировал тебя!''', parse_mode='html')
+    bot.register_next_step_handler(msg, process_first_name_step)
+
+def process_first_name_step(message):
+    try:
+        user_id = message.from_user.id
+        user_data[user_id] = User(message.text)
+        msg = bot.send_message(message.chat.id, 'Отлично, а теперь введи Фамилию!')
+        bot.register_next_step_handler(msg, process_last_name_step)
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Что-то пошло не так...')
+
+def process_last_name_step(message):
+    try:
+        user_id = message.from_user.id
+        user = user_data[user_id]
+        user.last_name = message.text
+        bot.send_message(message.chat.id, 'Регистрация успешна!')
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Что-то явно пошло не так?')
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
